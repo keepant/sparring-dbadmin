3a267c111b06d05dc0cb6c33a8c72b4392c441f8
Irfan Dwi Prasetyo <keepant@gmail.com>
2020-07-03 14:45:22 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# sparring-dbadmin
+sparring app dashboard admin
+
+Template by:  [Devias Kit - Admin Dashboard](https://react-material-dashboard.devias.io/dashboard)
---
+++ src/views/Dashboard/components/index.js
@@ -0,0 +1,6 @@
+export { default as Owner } from './Owner';
+export { default as LatestSales } from './LatestSales';
+export { default as TasksProgress } from './TasksProgress';
+export { default as TotalProfit } from './TotalProfit';
+export { default as TotalUsers } from './TotalUsers';
+export { default as UsersByDevice } from './UsersByDevice';
